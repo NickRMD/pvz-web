e4c655b314429fb9630d3dc19fa3c69817cf7fb4
Nick <nicolasrenanmachado@gmail.com>
2025-05-14 05:16:18 -0300

    Started working on the sound system

--- src/gameSystems/SoundSystem.ts
+++ src/gameSystems/SoundSystem.ts
@@ -1,10 +1,37 @@
-const ost1 = new Audio("assets/ost_1.m4a");
-const ost2 = new Audio("assets/ost_menu.m4a");
+enum InGameSongs {
+  
+}
 
-function playStartSound() {
-	ost1.play().catch((err) => console.error("Erro ao tocar o som:", err));
-	if (ost1.ended || ost1.paused || ost1.currentTime === ost1.duration) {
-		ost1.currentTime = 0;
-		ost2.play();
-	}
+export default class SoundSystem {
+
+  private _in_game_soundtrack?: HTMLAudioElement[];
+  private _current_song: number = 0;
+  private _audio_worker = new Worker("./SoundWorker.ts");
+  private _music_timestamp_paused = 0;
+  private _playing = false;
+
+  constructor() {
+
+  }
+
+  public async load() {
+    const in_game_soundtrack = [];
+
+    in_game_soundtrack.push(new Audio("../../assets/ost_1.m4a"));
+    in_game_soundtrack.push(new Audio("../../assets/ost_menu.m4a"));
+
+    this._in_game_soundtrack = in_game_soundtrack;
+  }
+
+  public play_in_game_music() {
+    this._in_game_soundtrack![0].onended
+  }
+
+  public toggle_pause() {
+
+  }
+
+  public stop() {
+
+  }
 }
---
+++ src/gameSystems/SoundWorker.ts
@@ -0,0 +1,3 @@
+self.onmessage = (e: MessageEvent<number>) => {
+  
+}
