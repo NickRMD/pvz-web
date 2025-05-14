0434666ec7eee38e15ae7d4f0f72887e61717fb5
Nick <nicolasrenanmachado@gmail.com>
2025-05-14 05:09:24 -0300

    TODO: Implement EntityLoader

---
+++ src/entities/EntityLoader.ts
@@ -0,0 +1,15 @@
+import type { Sprites } from "../SpriteLoader";
+
+enum Difficulty {
+  Easy = 0,
+  Normal,
+  Hard
+}
+
+class EntityLoader {
+  constructor(sprites: Readonly<Sprites>,difficulty: Difficulty) {
+    
+  }
+}
+
+export default EntityLoader;
