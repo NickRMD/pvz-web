8ece53e9b51e823dc089faf6245541e6d1691ad2
Nick <nicolasrenanmachado@gmail.com>
2025-05-24 03:12:33 -0300

    added utility function

--- src/gameSystems/WaveSystem.ts
+++ src/gameSystems/WaveSystem.ts
@@ -1,128 +1,141 @@
-import { ZombieType } from "../models/zombieTypes";
+import ZombieKind from "../entities/entityKinds/ZombieKind";
 
 class WaveSystem {
-	private _add_zombie: (row: number, zombie_type: ZombieType) => void;
-	private _waves: { zombies: number; types: ZombieType[]; interval: number }[] =
-		[];
-	private _level = 0;
-	private _current_wave = 0;
-	private _zombies_spawned = 0;
-	private _zombies_in_wave = 0;
-	private _wave_cooldown = 30000;
-	private _last_wave_time = 0;
-	private _wave_active = false;
-	private _rows: () => number;
+  private _add_zombie: (row: number, zombie_type: ZombieKind) => void;
+  private _waves: { zombies: number; types: ZombieKind[]; interval: number }[] =
+    [];
+  private _level = 0;
+  private _current_wave = 0;
+  private _zombies_spawned = 0;
+  private _zombies_in_wave = 0;
+  private _wave_cooldown = 30000;
+  private _last_wave_time = 0;
+  private _wave_active = false;
+  private _rows: () => number;
 
-	constructor(
-		rows: () => number,
-		add_zombie: (row: number, zombie_type: ZombieType) => void,
-	) {
-		this._rows = rows;
-		this._add_zombie = add_zombie;
-		this.initialize_waves();
-	}
+  constructor(
+    rows: () => number,
+    add_zombie: (row: number, zombie_type: ZombieKind) => void,
+  ) {
+    this._rows = rows;
+    this._add_zombie = add_zombie;
+    this.initialize_waves();
+  }
 
-	private _level_up() {
-		this._level++;
-	}
+  public level(): Readonly<number> {
+    return this._level;
+  }
 
-	initialize_waves() {
-		this._waves = [
-			{ zombies: 5, types: [ZombieType.basic], interval: 3000 },
-			{
-				zombies: 8,
-				types: [ZombieType.basic, ZombieType.cone],
-				interval: 2500,
-			},
-			{
-				zombies: 12,
-				types: [ZombieType.basic, ZombieType.cone, ZombieType.bucket],
-				interval: 2000,
-			},
-			{
-				zombies: 15,
-				types: [ZombieType.basic, ZombieType.cone, ZombieType.bucket],
-				interval: 1500,
-			},
-			{
-				zombies: 20,
-				types: [ZombieType.basic, ZombieType.cone, ZombieType.bucket],
-				interval: 1000,
-			},
-		];
-	}
+  private _level_up() {
+    this._level++;
+  }
 
-	update(timestamp: number, game_over: boolean, paused: boolean) {
-		if (game_over || paused) return;
+  initialize_waves() {
+    this._waves = [
+      { zombies: 5, types: [ZombieKind.Basic], interval: 3000 },
+      {
+        zombies: 8,
+        types: [ZombieKind.Basic, ZombieKind.Cone],
+        interval: 2500,
+      },
+      {
+        zombies: 12,
+        types: [ZombieKind.Basic, ZombieKind.Cone, ZombieKind.Bucket],
+        interval: 2000,
+      },
+      {
+        zombies: 15,
+        types: [ZombieKind.Basic, ZombieKind.Cone, ZombieKind.Bucket],
+        interval: 1500,
+      },
+      {
+        zombies: 20,
+        types: [ZombieKind.Basic, ZombieKind.Cone, ZombieKind.Bucket],
+        interval: 1000,
+      },
+    ];
+  }
 
-		if (
-			!this._wave_active &&
-			timestamp - this._last_wave_time > this._wave_cooldown
-		) {
-			this.start_next_wave(timestamp);
-		}
+  public update(timestamp: number, game_over: boolean, paused: boolean) {
+    if (game_over || paused) return;
 
-		if (this._wave_active && this._zombies_spawned < this._zombies_in_wave) {
-			if (
-				timestamp - this._last_wave_time >
-				this._waves[this._current_wave].interval
-			) {
-				this.spawn_zombie();
-				this._last_wave_time = timestamp;
-				this._zombies_spawned++;
+    if (
+      !this._wave_active &&
+      timestamp - this._last_wave_time > this._wave_cooldown
+    ) {
+      this.start_next_wave(timestamp);
+    }
 
-				if (this._zombies_spawned >= this._zombies_in_wave) {
-					this.end_wave();
-				}
-			}
-		}
-	}
+    if (this._wave_active && this._zombies_spawned < this._zombies_in_wave) {
+      if (
+        timestamp - this._last_wave_time >
+        this._waves[this._current_wave].interval
+      ) {
+        this.spawn_zombie();
+        this._last_wave_time = timestamp;
+        this._zombies_spawned++;
 
-	start_next_wave(timestamp: number) {
-		if (this._current_wave >= this._waves.length) {
-			this._current_wave = 0;
-			this._level_up();
-			this.increase_difficulty();
-		}
+        if (this._zombies_spawned >= this._zombies_in_wave) {
+          this.end_wave();
+        }
+      }
+    }
+  }
 
-		this._wave_active = true;
-		this._zombies_spawned = 0;
-		this._zombies_in_wave =
-			this._waves[this._current_wave].zombies + Math.floor(this._level / 2);
-		this._last_wave_time = timestamp;
+  start_next_wave(timestamp: number) {
+    if (this._current_wave >= this._waves.length) {
+      this._current_wave = 0;
+      this._level_up();
+      this.increase_difficulty();
+    }
 
-		console.log(
-			`Wave ${this._current_wave + 1} iniciada! Nível ${this._level}`,
-		);
-	}
+    this._wave_active = true;
+    this._zombies_spawned = 0;
+    this._zombies_in_wave =
+      this._waves[this._current_wave].zombies + Math.floor(this._level / 2);
+    this._last_wave_time = timestamp;
 
-	end_wave() {
-		this._wave_active = false;
-		this._last_wave_time = performance.now();
-		this._current_wave++;
-		// gameState.waveCount++;
+    console.log(
+      `Wave ${this._current_wave + 1} iniciada! Nível ${this._level}`,
+    );
+  }
 
-		console.log(
-			`Wave completada! Próxima wave em ${this._wave_cooldown / 1000} segundos.`,
-		);
-	}
+  end_wave() {
+    this._wave_active = false;
+    this._last_wave_time = performance.now();
+    this._current_wave++;
+    // gameState.waveCount++;
 
-	public current_wave() {}
+    console.log(
+      `Wave completa! Próxima wave em ${this._wave_cooldown / 1000} segundos.`,
+    );
+  }
 
-	private spawn_zombie() {
-		const wave = this._waves[this._current_wave % this._waves.length];
-		const zombieType =
-			wave.types[Math.floor(Math.random() * wave.types.length)];
-		const row = Math.floor(Math.random() * this._rows());
-		this._add_zombie(row, zombieType);
-	}
+  public current_wave(): Readonly<number> {
+    return this._current_wave;
+  }
 
-	increase_difficulty() {
-		this._wave_cooldown = Math.max(10000, this._wave_cooldown - 2000);
-		this._waves.forEach((wave) => {
-			wave.interval = Math.max(1000, wave.interval - 200);
-		});
-	}
+  private spawn_zombie() {
+    const wave = this._waves[this._current_wave % this._waves.length];
+    const zombieType =
+      wave.types[Math.floor(Math.random() * wave.types.length)];
+    const row = Math.floor(Math.random() * this._rows());
+    this._add_zombie(row, zombieType);
+  }
+
+  public reset() {
+    this._current_wave = 0;
+    this._zombies_spawned = 0;
+    this._wave_active = false;
+    this._last_wave_time = 0;
+  }
+
+  increase_difficulty() {
+    this._wave_cooldown = Math.max(10000, this._wave_cooldown - 2000);
+    for (const wave of this._waves) {
+      wave.interval = Math.max(1000, wave.interval - 200);
+    }
+  }
 }
 
 export default WaveSystem;
