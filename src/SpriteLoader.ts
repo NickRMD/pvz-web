d721403be99d1b8eff516e22715f1c135ef1f720
Nick <nicolasrenanmachado@gmail.com>
2025-05-24 03:15:24 -0300

    Changed zombie to zombiebasic and added a function that returns a SpriteKeyEnum if the string is a sprite key

--- src/SpriteLoader.ts
+++ src/SpriteLoader.ts
@@ -3,46 +3,54 @@ export enum SpriteKeyEnum {
   Peashooter = "peashooter",
   Pea = "pea",
   Walnut = "walnut",
-  Zombie = "zombie_basic",
   Sun = "sun",
+  ZombieBasic = "zombie_basic",
   ZombieCone = "zombie_cone",
   ZombieBucket = "zombie_bucket",
-};
+}
 
 type SpriteKey = `${SpriteKeyEnum}`;
 export interface Sprites extends Record<SpriteKey, HTMLImageElement> {}
 
 export default class SpriteLoader {
-	private _sprites?: Sprites;
-	private _loaded = false;
+  private _sprites?: Sprites;
+  private _loaded = false;
 
-	constructor() {}
-
-	public async load() {
-    const spriteKeys = Object.values(SpriteKeyEnum);	
-    const spritesArray = await Promise.all(spriteKeys.map(async key => {
-      const img = new Image();
-      const { default: url } = await import(`#/sprites/${key}.webp`);
-      img.src = url;
-      await new Promise<void>((resolve, reject) => {
-        img.onload = () => resolve();
-        img.onerror = () => reject(new Error(`Failed to load sprite ${key} at ${url}`));
-      })
-      return [key, img] as const; 
-    }));
+  public async load() {
+    const spriteKeys = Object.values(SpriteKeyEnum);
+    const spritesArray = await Promise.all(
+      spriteKeys.map(async (key) => {
+        const img = new Image();
+        const { default: url } = await import(`#/sprites/${key}.webp`);
+        img.src = url;
+        await new Promise<void>((resolve, reject) => {
+          img.onload = () => resolve();
+          img.onerror = () =>
+            reject(new Error(`Failed to load sprite ${key} at ${url}`));
+        });
+        return [key, img] as const;
+      }),
+    );
 
     const sprites: Sprites = Object.fromEntries(spritesArray) as Sprites;
 
-		this._loaded = true;
-		this._sprites = sprites;
-	}
+    this._loaded = true;
+    this._sprites = sprites;
+  }
 
-	public is_loaded(): Readonly<boolean> {
-		return this._loaded;
-	}
+  public static isSprite(value: string): SpriteKeyEnum {
+    if (Object.values(SpriteKeyEnum).includes(value as SpriteKeyEnum)) {
+      return value as SpriteKeyEnum;
+    }
+    throw new Error("Value is not a valid Sprite");
+  }
 
-	public sprites(): Readonly<Sprites> {
-		if (!this._sprites) throw Error("Sprites aren't initialized yet!")
-      else return this._sprites;
-	}
+  public is_loaded(): Readonly<boolean> {
+    return this._loaded;
+  }
+
+  public sprites(): Readonly<Sprites> {
+    if (!this._sprites) throw Error("Sprites aren't initialized yet!");
+    return this._sprites;
+  }
 }
