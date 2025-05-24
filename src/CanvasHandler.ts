04351a89f4dde3ab354b6321f167803416bd938d
Nick <nicolasrenanmachado@gmail.com>
2025-05-24 03:20:33 -0300

    Formatted

--- src/CanvasHandler.ts
+++ src/CanvasHandler.ts
@@ -1,38 +1,38 @@
 class CanvasHandler {
-	private _canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-	private _ctx = this._canvas.getContext("2d")!;
-	private _grass_width = this._canvas.width * 0.8;
-	private _zombie_area_width = this._canvas.width * 0.2;
+  private _canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+  private _ctx = this._canvas.getContext("2d") as CanvasRenderingContext2D;
+  private _grass_width = this._canvas.width * 0.8;
+  private _zombie_area_width = this._canvas.width * 0.2;
 
-	constructor() {
-		this._ctx.imageSmoothingEnabled = false;
-		this._canvas_resize();
-		window.addEventListener("resize", this._canvas_resize);
-	}
+  constructor() {
+    this._ctx.imageSmoothingEnabled = false;
+    this._canvas_resize();
+    window.addEventListener("resize", this._canvas_resize.bind(this));
+  }
 
-	private _canvas_resize() {
-		this._canvas.width = window.innerWidth;
-		this._canvas.height = window.innerHeight;
+  private _canvas_resize() {
+    this._canvas.width = window.innerWidth;
+    this._canvas.height = window.innerHeight;
     this._grass_width = this._canvas.width * 0.8;
     this._zombie_area_width = this._canvas.width * 0.2;
-		this.draw_background();
-	}
+    this.draw_background();
+  }
 
-	public canvas(): Readonly<typeof this._canvas> {
-		return this._canvas;
-	}
+  public canvas(): Readonly<typeof this._canvas> {
+    return this._canvas;
+  }
 
-	public ctx(): Readonly<typeof this._ctx> {
-		return this._ctx;
-	}
+  public ctx(): Readonly<typeof this._ctx> {
+    return this._ctx;
+  }
 
-	public mut_ctx() {
-		return this._ctx;
-	}
+  public mut_ctx() {
+    return this._ctx;
+  }
 
-	public mut_canvas() {
-		return this._canvas;
-	}
+  public mut_canvas() {
+    return this._canvas;
+  }
 
   public grass_width(): Readonly<number> {
     return this._grass_width;
@@ -42,18 +42,18 @@ class CanvasHandler {
     return this._zombie_area_width;
   }
 
-	public draw_background() {
-		this._ctx.fillStyle = "#7CFC0000";
-		this._ctx.fillRect(0, 0, this._grass_width, this._canvas.height);
+  public draw_background() {
+    this._ctx.fillStyle = "#7CFC0000";
+    this._ctx.fillRect(0, 0, this._grass_width, this._canvas.height);
 
-		this._ctx.fillStyle = "#7CFC0000";
-		this._ctx.fillRect(
-			this._grass_width,
-			0,
-			this._zombie_area_width,
-			this._canvas.height,
-		);
-	}
+    this._ctx.fillStyle = "#7CFC0000";
+    this._ctx.fillRect(
+      this._grass_width,
+      0,
+      this._zombie_area_width,
+      this._canvas.height,
+    );
+  }
 }
 
 export default CanvasHandler;
