d02a911615824be41dd458cc31e45d844ac82734
Nick <nicolasrenanmachado@gmail.com>
2025-05-24 03:29:59 -0300

    Changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { AliasOptions, defineConfig } from "vite";
-import path from "path";
-import { fileURLToPath } from 'url';
+import { type AliasOptions, defineConfig } from "vite";
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
