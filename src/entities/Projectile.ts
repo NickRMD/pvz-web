1a711fc5bec142bc8626c36fd3926d8b028115d9
Nick <nicolasrenanmachado@gmail.com>
2025-05-24 03:20:11 -0300

    Projectile is now an abstract class so there could be all sorts of projectiles with different stats and functionality, TODO: add projectiles that do damage to plants

--- src/entities/Projectile.ts
+++ src/entities/Projectile.ts
@@ -1,73 +1,82 @@
-class Projectile {
-	constructor(x, y, targetX, targetY, damage, speed, type = "basic") {
-		this.x = x;
-		this.y = y;
-		this.targetX = targetX;
-		this.targetY = targetY;
-		this.damage = damage;
-		this.speed = speed;
-		this.type = type;
-		this.reached = false;
-		this.width = 20;
-		this.height = 20;
-		this.angle = Math.atan2(targetY - y, targetX - x);
-	}
+import type GameState from "../GameState";
+import Signal from "../utils/Signal";
+import Entity from "./Entity";
 
-	update() {
-		const dx = this.targetX - this.x;
-		const dy = this.targetY - this.y;
-		const distance = Math.sqrt(dx * dx + dy * dy);
+abstract class Projectile extends Entity {
+  protected _target_x: Signal<number>;
+  protected _target_y: Signal<number>;
+  protected _damage: number;
+  private _speed: number;
+  protected _reached = false;
+  private _angle: number;
 
-		if (distance < this.speed) {
-			this.reached = true;
-			this.hitTarget();
-		} else {
-			this.x += (dx / distance) * this.speed;
-			this.y += (dy / distance) * this.speed;
-		}
-	}
+  protected _health: undefined;
+  protected _max_health: undefined;
 
-	hitTarget() {
-		gameState.zombies.forEach((zombie) => {
-			const dx = this.x - zombie.x;
-			const dy =
-				this.y -
-				(gameState.grid.offsetY +
-					zombie.row * gameState.grid.cellHeight +
-					gameState.grid.cellHeight / 2);
-			const distance = Math.sqrt(dx * dx + dy * dy);
+  constructor(
+    x: number,
+    y: number,
+    target_x: number,
+    target_y: number,
+    damage: number,
+    speed: number,
+    game_state: GameState,
+    /*type = "basic"*/
+  ) {
+    super(game_state);
+    this._x = new Signal(x);
+    this._y = new Signal(y);
+    this._target_x = new Signal(target_x);
+    this._target_y = new Signal(target_y);
+    this._damage = damage;
+    this._speed = speed;
+    // this.type = type;
+    this._angle = Math.atan2(target_y - y, target_x - x);
+  }
 
-			if (distance < this.width / 2 + zombie.width / 2) {
-				zombie.health -= this.damage;
-				if (zombie.health <= 0) {
-					gameState.zombies.splice(gameState.zombies.indexOf(zombie), 1);
-					updateUI();
-				}
-				this.reached = true;
-			}
-		});
-	}
+  public update() {
+    const dx = this._target_x.value - this._x.value;
+    const dy = this._target_y.value - this._y.value;
+    const distance = Math.sqrt(dx ** 2 + dy ** 2);
 
-	draw(ctx) {
-		if (sprites.projectile.complete) {
-			ctx.save();
-			ctx.translate(this.x, this.y);
-			ctx.rotate(this.angle);
-			ctx.drawImage(
-				sprites.projectile,
-				-this.width / 2,
-				-this.height / 2,
-				this.width,
-				this.height,
-			);
-			ctx.restore();
-		} else {
-			ctx.fillStyle = "#FF5722";
-			ctx.beginPath();
-			ctx.arc(this.x, this.y, 5, 0, Math.PI * 2);
-			ctx.fill();
-		}
-	}
+    if (distance < this._speed) {
+      this._reached = true;
+      this._hit_target();
+    } else {
+      // this._x.value += ((dx / distance) * this._speed);
+      // this._y.value += ((dy / distance) * this._speed);
+      this._x.value += (dx / distance) * this._speed;
+      this._y.value += (dy / distance) * this._speed;
+    }
+  }
+
+  public reached(): Readonly<boolean> {
+    return this._reached;
+  }
+
+  protected abstract _hit_target(): void;
+
+  public draw(ctx: CanvasRenderingContext2D) {
+    const sprite = this._game_state.sprite_loader.sprites()[this._sprite];
+    if (sprite.complete) {
+      ctx.save();
+      ctx.translate(this._x.value, this._y.value);
+      ctx.rotate(this._angle);
+      ctx.drawImage(
+        sprite,
+        -this._width / 2,
+        -this._height / 2,
+        this._width,
+        this._height,
+      );
+      ctx.restore();
+    } else {
+      ctx.fillStyle = this._hex_color;
+      ctx.beginPath();
+      ctx.arc(this._x.value, this._y.value, 5, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }
 }
 
 export default Projectile;
