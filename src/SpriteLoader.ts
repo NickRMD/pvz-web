1aeb5fa736b016bd6a3a20a00c44f29c253f8b0d
Nick <nicolasrenanmachado@gmail.com>
2025-05-26 03:55:16 -0300

    Added a non conflicting cache mechanism and added sprite loader to window for debugging, TODO: simplify caching process

--- src/SpriteLoader.ts
+++ src/SpriteLoader.ts
@@ -7,20 +7,38 @@ export enum SpriteKeyEnum {
   ZombieBasic = "zombie_basic",
   ZombieCone = "zombie_cone",
   ZombieBucket = "zombie_bucket",
+};
+
+declare global {
+  interface Window {
+    _sprite_loader: SpriteLoader
+  }
 }
 
 type SpriteKey = `${SpriteKeyEnum}`;
 export interface Sprites extends Record<SpriteKey, HTMLImageElement> {}
+export interface SpriteCache extends Partial<Record<SpriteKey, HTMLCanvasElement>> {}
 
 export default class SpriteLoader {
   private _sprites?: Sprites;
+  private _sprite_cache: SpriteCache = {};
   private _loaded = false;
+
+  constructor() {
+    if(import.meta.env.DEV) {
+      window._sprite_loader = this;
+    }
+  }
 
   public async load() {
     const spriteKeys = Object.values(SpriteKeyEnum);
     const spritesArray = await Promise.all(
       spriteKeys.map(async (key) => {
         const img = new Image();
+        
+        // Used so the image doesn't flicker when loaded
+        img.style.display = "none";
+        
         const { default: url } = await import(`#/sprites/${key}.webp`);
         img.src = url;
         await new Promise<void>((resolve, reject) => {
@@ -38,6 +56,12 @@ export default class SpriteLoader {
     this._sprites = sprites;
   }
 
+  public set_sprite(sprite_key: SpriteKeyEnum, new_image: HTMLCanvasElement) {
+    if (!this._sprites) throw Error("Sprites aren't initialized yet!");
+    console.log(`${sprite_key} cached!`)
+    this._sprite_cache[sprite_key] = new_image;
+  }
+
   public static isSprite(value: string): SpriteKeyEnum {
     if (Object.values(SpriteKeyEnum).includes(value as SpriteKeyEnum)) {
       return value as SpriteKeyEnum;
@@ -47,6 +71,10 @@ export default class SpriteLoader {
 
   public is_loaded(): Readonly<boolean> {
     return this._loaded;
+  }
+
+  public cached_sprites(): Readonly<SpriteCache> {
+    return this._sprite_cache;
   }
 
   public sprites(): Readonly<Sprites> {
