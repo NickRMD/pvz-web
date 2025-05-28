a352f29aadecd30a04ac1cffba13224654fca044
Nick <nicolasrenanmachado@gmail.com>
2025-05-28 04:57:58 -0300

    Added watch for, for watching for n times, not infinitely like watch

--- src/utils/debug/watch.ts
+++ src/utils/debug/watch.ts
@@ -1,6 +1,7 @@
 declare global {
   interface Window {
     watch: (watching: () => unknown, every_ms: number) => void;
+    watch_for: (watching: () => unknown, every_ms: number, turns: number) => void;
   }
 }
 
@@ -10,3 +11,11 @@ window.watch = (watching, every) => {
     window.watch(watching, every);
   }, every);
 };
+
+window.watch_for = (watching, every, turns) => {
+  setTimeout(() => {
+    if(turns === 0) return;
+    watching();
+    window.watch_for(watching, every, turns - 1);
+  }, every)
+}
